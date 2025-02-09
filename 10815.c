195d9982f11e378b025cf0de14b46575abf2ba68
soeun-727 <soeun2215562@sm.ac.kr>
2025-02-10 01:55:59 +0900

    dsafd

--- 10815.c
+++ 10815.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
   int N;
@@ -8,31 +9,35 @@ int main(){
   
   
   scanf("%d\n", &N);
-  long *arr1 = malloc(N);
+  long *arr1 = (long *)malloc(N*sizeof(long));
   for(int i=0; i<N; i++){
     scanf("%lu ", &n);
     arr1[i]=n;
   }
 
   scanf("%d\n", &M);
-  long *arr2 = malloc(M);
+  long *arr2 = (long *)malloc(M*sizeof(long));
   for(int i = 0; i<M; i++){
     scanf( "%lu ", &m);
     arr2[i]= m;
   }
 
-  for (int i = 0; i<N; i++){
-    for(int j = 0; j<M; j++){
-      if( arr1[i] == arr2[j]){
+for (int i = 0; i < N; i++) {
+  for (int j = 0; j < M; j++){
+    if (arr1[i] == arr2[j]) {
         arr1[i] = 1;
-      }
-      else{
-        arr1[i] = 0;
-      }
+        break;
+    } else {
+        continue;
     }
   }
+  arr1[i] = 0;
+}
 
   for (int i = 0; i<N;i++){
     printf("%lu ", arr1[i]);
   }
+
+  free(arr1); free(arr2);
+  return 0;
 }
