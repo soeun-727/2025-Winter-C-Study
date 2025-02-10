09e41f8d4386caea3797eaefb53c3ae7f45e5d61
soeun-727 <soeun2215562@sm.ac.kr>
2025-02-10 13:06:13 +0900

    5주차_c언어_25.5기_박소은

--- 10815.c
+++ 10815.c
@@ -1,43 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-  int N;
-  long n =0;
-  int M;
-  long m = 0;
-  
-  
-  scanf("%d\n", &N);
-  long *arr1 = (long *)malloc(N*sizeof(long));
-  for(int i=0; i<N; i++){
-    scanf("%lu ", &n);
-    arr1[i]=n;
-  }
-
-  scanf("%d\n", &M);
-  long *arr2 = (long *)malloc(M*sizeof(long));
-  for(int i = 0; i<M; i++){
-    scanf( "%lu ", &m);
-    arr2[i]= m;
-  }
-
-for (int i = 0; i < N; i++) {
-  for (int j = 0; j < M; j++){
-    if (arr1[i] == arr2[j]) {
-        arr1[i] = 1;
-        break;
-    } else {
-        continue;
-    }
-  }
-  arr1[i] = 0;
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
-  for (int i = 0; i<N;i++){
-    printf("%lu ", arr1[i]);
-  }
+int main() {
+    int N, M;
+    scanf("%d", &N);
+    
+    int *arr1 = (int *)malloc(N * sizeof(int));
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr1[i]);
+    }
 
-  free(arr1); free(arr2);
-  return 0;
+    qsort(arr1, N, sizeof(int), compare);
+
+    scanf("%d", &M);
+    
+    int *result = (int *)malloc(M * sizeof(int));
+
+    for (int i = 0; i < M; i++) {
+        int target;
+        scanf("%d", &target);
+        
+        if (bsearch(&target, arr1, N, sizeof(int), compare)) {
+            result[i] = 1;
+        } else {
+            result[i] = 0;
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        printf("%d ", result[i]);
+    }
+
+    free(arr1);
+    free(result);
+    return 0;
 }
